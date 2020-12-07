61685201bdb965841f0dd98fe6b0864316647893
Pavanbn03 <pavanbn03@gmail.com>
2020-12-07 13:52:41 +0530

    adding and removing cart items in chekout page implemented

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,5 +1,6 @@
 import { CartActionTypes } from "./cardTypes";
-import {addItemToCart} from './cartUtilits'
+import { addItemToCart } from "./cartUtilits";
+import { removeItemFromCart } from "./cartUtilits";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -16,8 +17,20 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: addItemToCart(state.cartItems,action.payload)
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
+      };
+      case CartActionTypes.REMOVE_CART_ITEM:
+        return{
+          ...state,
+          cartItems: removeItemFromCart(state.cartItems,action.payload)
+        }
     default:
       return state;
   }
